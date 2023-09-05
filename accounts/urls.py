6c164dcf3cd32528f349744e376c281078ae2eb1
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-09-05 12:07:45 +0300

    about developer page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dr_nagib_cv
+from .views import dr_nagib_cv, about_developer
 
 app_name = "accounts"
 
 urlpatterns = [
-    path('dr.mohamed-nagib', dr_nagib_cv, name="dr_nagib_cv")
+    path('dr.mohamed-nagib', dr_nagib_cv, name="dr_nagib_cv"),
+    path('about_developer', about_developer, name="about_developer")
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect
 
+
 def dr_nagib_cv(request):
     return render(request, 'dr_nagib.html', context=None)
+
+
+def about_developer(request):
+    return render(request, 'about_developer.html', context=None)
